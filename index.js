2ba6231b20d445aaa8050528f8c2174fe37f744c
dadiprasanth <dadiprasanth24@gmail.com>
2022-09-24 13:32:49 +0530

    cors changed to top

--- index.js
+++ index.js
@@ -5,7 +5,8 @@ const property=require("./routes/propertyroute")
 const user=require('./routes/userReg')
 const cors=require("cors")
 const app=express()
-const port=process.env.PORT||8080  
+const port=process.env.PORT||8080 
+app.use(cors()) 
 app.listen(port,()=>console.log(`app is listening at${port}`))
 mongoose.connect('mongodb+srv://dadiprasanth24:9963753437@cluster0.gglcimt.mongodb.net/realestate',err=>{
     if(err){
@@ -14,7 +15,7 @@ mongoose.connect('mongodb+srv://dadiprasanth24:9963753437@cluster0.gglcimt.mongo
         console.log("connected to database")
     }
 })
-app.use(cors())
+
 app.use(bodyparser.json())
 app.use("/property",property) 
 app.use("/users",user)
